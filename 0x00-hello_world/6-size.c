967dd24576b0320e02349f533ccb2c463917c43e
Joy <nwosuchinaza22@gmail.com>
2023-04-22 04:53:33 -0700

    6 size c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main -Program that print a line using printf function
  * Return: 0
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-
+}
