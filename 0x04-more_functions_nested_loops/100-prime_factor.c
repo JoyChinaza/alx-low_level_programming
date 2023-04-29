d602c29d9f53146b62cf7d24b6cd14139f73fc44
Joy <nwosuchinaza22@gmail.com>
2023-04-29 12:12:36 -0700

    100 prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-
 /**
  * main - prints prime factors of 612852475143
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	unsigned long int i, n = 612852475143;
-
 	for (i = 3; i < 782849; i = i + 2)
 	{
 		while ((n % i == 0) && (n != i))
 			n = n / i;
 	}
 	printf("%lu\n", n);
-	return (0);
+	return (0);
 }
