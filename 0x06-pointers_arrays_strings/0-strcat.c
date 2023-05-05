4edf3c4f37845578c25386f2dde85350fb5043e3
Joy <nwosuchinaza22@gmail.com>
2023-05-04 19:24:05 -0700

    0 strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // a fuction that catenates two strings
+   char destination[] = "*src";
+    char source[] = "*dest";
+    strcat(destination,source);
+    printf("Concatenated String: %s\n", destination);
+   
+    write("src dest");
+
+    return 0;
+}
