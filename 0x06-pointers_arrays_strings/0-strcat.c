8e42bb5038c97c4884689302e8808370d3279c9c
Joy <nwosuchinaza22@gmail.com>
2023-05-04 19:45:31 -0700

    0 strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     // a fuction that catenates two strings
    char destination[] = "*src";
     char source[] = "*dest";
     strcat(destination,source);
-    printf("Concatenated String: %s\n", destination);
+    printf ("Concatenated String: %s\n", destination);
    
-    write("src dest");
+    write ("src dest");
 
     return 0;
 }
