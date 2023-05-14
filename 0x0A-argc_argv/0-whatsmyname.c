9f4ccea8b73fd69b93bc9c23cfa5f7efde637b9f
Joy <nwosuchinaza22@gmail.com>
2023-05-13 22:26:22 -0700

    task zero

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,17 +3,8 @@
 
 int main(int argc, char* argv[])
 {
-	int i;
-	for (i=0;i < argc;i++)
-	{
-		printf("%s ", argv[i]);
+if (argc > 0)
+printf ("%s\n", argv[0]);
 
-	}
-	printf("\n");
-
-	if (argc > 0)
-	printf ("%s\n", argv[0]);
-
-	return (0);
-
+return (0);
 }
