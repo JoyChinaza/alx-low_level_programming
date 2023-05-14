f409884653107dd52437553fe32ffd004b07eaae
Joy <nwosuchinaza22@gmail.com>
2023-05-13 22:29:20 -0700

    task zero

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
-#include"main.h"
+#include "main.h"
 
-int main(int argc, char* argv[])
+
+/**
+* main - print name of program
+* @argc: argument counter
+* @argv: pointer to array of arguments
+* Return: 0 on success
+*/
+
+int main(int argc, char *argv[])
 {
 if (argc > 0)
-printf ("%s\n", argv[0]);
+printf("%s\n", argv[0]);
 
 return (0);
 }
