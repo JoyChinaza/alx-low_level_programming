1976ca36dd28da4b046480ddd98a4fbb5da19655
Joy <nwosuchinaza22@gmail.com>
2023-05-22 17:39:04 -0700

    init dogu

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,12 +4,12 @@
  * init_dog - initializes a structure of type dog
  * @d: pointer to structure
  * @name: pointer to the name of the dog
- * @age:pointer to the age ofthe  dog
- * @owner: pointer to owner of the dog
+ * @age: age of dog
+ * @owner: pointer to the owner of the  dog
  *
  * Return: void
  */
-void init_dog(struct dog *d, char *name, float *age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
