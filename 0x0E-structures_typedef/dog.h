57ec2b7fcb94bdc3a50bb089eadc24f70a704708
Joy <nwosuchinaza22@gmail.com>
2023-05-22 18:05:28 -0700

    dog h 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,34 +1,19 @@
-#ifndef DOG
-#define DOG
-
-include <stdio.h>
-include main.h
-include "dog. h"
-
+#include "dog.h"
+#include <stdlib.h>
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * init_dog - the initializes a variable of type struct dog.
+ * @d: the  pointer to struct
+ * @name: the  parameter of name of the dog
+ * @age: the parameter of age of the dog
+ * @owner: the  pinter parameter to owner of the dog
  */
-int main(void)
-
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
-struct dog my_dog;
-my_dog.name = "Poppy";
-my_dog.age = 3.5;
-my_dog.owner = "Bob";
-printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-return (0);
-struct dog my_dog;
-char *name;
-int  age;
-};
-
-int main(void)
-{ struct dog my_dog;
-user.name = "poppy";
-return (0);
-}
-
-#endif
+		if (d == NULL)
+		{
+			return;
+		}
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+}f
