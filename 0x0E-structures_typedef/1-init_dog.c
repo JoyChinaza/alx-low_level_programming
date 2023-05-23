c2120f09124fa331d95ccd8e7494053537ed78e4
Joy <nwosuchinaza22@gmail.com>
2023-05-22 17:30:14 -0700

    init dogs

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,19 +1,3 @@
-#include <stdio.h>
-#include "dog.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    struct dog my_dog;
-
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
-}
 #include "dog.h"
 
 /**
@@ -25,11 +9,11 @@ int main(void)
  *
  * Return: void
  */
-void init_dog(struct dog *d, char *name, float *age, char *owner)
+void init_dog (struct dog *d, char *name, float *age, char *owner)
 {
 	if (d)
 	{
-	d->name = name;
+	d- >name = name;
 	d->age = age;
 	d->owner = owner;
 	}
