11a5667554da4d09732f6b454fad5f32f1f80779
Joy <nwosuchinaza22@gmail.com>
2023-05-24 15:10:06 -0700

    prototypes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,9 @@
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+
+#include <stdlib.h>
+#include <stddef.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
@@ -7,3 +13,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
+#endif
