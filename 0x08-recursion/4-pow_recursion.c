38d603db9b5d7cb2393f5a2ff754282d9abfb4fe
Joy <nwosuchinaza22@gmail.com>
2023-05-26 22:33:34 -0700

    pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,34 +2,9 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-int r;
-
-r = _pow_recursion(1, 10);
-printf("%d\n", r);
-r = _pow_recursion(1024, 0);
-printf("%d\n", r);
-r = _pow_recursion(2, 16);
-printf("%d\n", r);
-r = _pow_recursion(5, 2);
-printf("%d\n", r);
-r = _pow_recursion(5, -2);
-printf("%d\n", r);
-r = _pow_recursion(-5, 3);
-printf("%d\n", r);
-return (0);
-}
-#include "main.h"
-
-/**
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: value to raise in pow recursion
- * @y: power to be raised to
+ * @y: power to be raised to in pow recursion
  *
  * Return: result of the power
  */
