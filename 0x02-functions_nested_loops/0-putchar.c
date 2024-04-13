fe79b7866843c8c8c1817984fcbfd75979f2584f
your username <nwosuchinaza22@gmail.com>
2024-04-13 07:45:18 -0700

    coding with vs code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-        putchar('_');
+	putchar('_');
 	putchar('p');
 	putchar('u');
-        putchar('t');
+	putchar('t');
 	putchar('c');
 	putchar('h');
 	putchar('a');
