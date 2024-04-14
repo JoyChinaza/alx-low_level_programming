b6d346702609dabc9e0a1c652885017c8bbf2510
your username <nwosuchinaza22@gmail.com>
2024-04-14 00:52:03 -0700

    print alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,20 +3,16 @@
 /**
  *   main - check the code.
  *   
- *    Return: Always 0.    
- *
+ *     Return: Always 0.
+ *    
  */
 
 int main(void)
 
-	char _alphabet(a,z)
-
+	char alphabet (a,z);
 {
-	char _alphabet a = 5
-	char _alphabet z = 10
 
-	if  _alphabet a < z
-	print_alphabet_x10();
+	    print_alphabet_x10();
 
 
 	        return (0);
