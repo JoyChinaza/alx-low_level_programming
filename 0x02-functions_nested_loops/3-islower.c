ff836df48ab33e53c1eb79da29a50cf7cd39500c
your username <nwosuchinaza22@gmail.com>
2024-05-21 21:47:35 -0700

    setting

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,24 +5,10 @@
  *Return: Always 0.
  */
 
-int main(void)
 
-{	
-
-char c;
-int _islower(int c);
-
+int _islower(int c)
 {
-
-	if (c >= 'a' && c <= 'z')
-
-{
-		return (1);
-}
-	else
-	{
-		return (0);
-	}
-}
+	if (c >= 97 && c <= 122)	
+        return (1);
 	return (0);
 }
