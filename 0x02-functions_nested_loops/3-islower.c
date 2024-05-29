852d7bc78a23e5f9d45a07a80917d5c1abaa4755
your username <nwosuchinaza22@gmail.com>
2024-05-29 13:05:29 -0700

    Make file 0

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,21 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * main - check the code.
- *Return: Always 0.
+ *_islower - function to check if
+ *character is lowercase
+ *
+ *@c: checks input of function
+ *
+ * checks for lower case 
+ * Lower cases are a-z
+ *Return: returns 1 if `c` is lowercase
+ *otherwise always 0 (Success)
  */
-
 
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)	
-        return (1);
+	if (c >= 97 && c <= 122)
+	return (1);
 	return (0);
 }
+
