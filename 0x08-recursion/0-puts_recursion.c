511c27bb36b625665d381fef3328837145e7a932
your username <nwosuchinaza22@gmail.com>
2024-06-14 20:48:51 -0700

    0 recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,17 @@
-#include "main.h" 
+#include "main.h"
 
 /**
- *  a function that prints a string, followed by a new line.
- *  this is recursion
- *
- */  
-
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
+ */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-		   
-    { 
-        _putchar('\n');
-        return;
-    }
-    _putchar(*s); 
-    _puts_recursion(s + 1);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
-
