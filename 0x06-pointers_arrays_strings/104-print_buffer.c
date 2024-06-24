d975a6fad0d0956e4e74b907ed5c406cd8bc50cd
your username <nwosuchinaza22@gmail.com>
2024-06-24 05:27:43 -0700

    104 print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,47 +1,43 @@
+#include "main.h"
 #include <stdio.h>
-#include <ctype.h>
 
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer.
+ * @size: size of buffer.
+ * Return: no return.
+ */
 void print_buffer(char *b, int size)
 {
-    int offset, byte;
+	int j, k, l;
 
-    if (size <= 0)
-    {
-        printf("\n");
-        return;
-    }
-
-    for (offset = 0; offset < size; offset += 10)
-    {
-        // Print the offset in hexadecimal (8 characters)
-        printf("%08x: ", offset);
-
-        // Print the hexadecimal content (2 chars each), 2 bytes at a time, separated by a space
-        for (byte = 0; byte < 10; byte++)
-        {
-            if (offset + byte < size)
-                printf("%02x", b[offset + byte]);
-            else
-                printf("  ");
-            
-            if (byte % 2)
-                printf(" ");
-        }
-
-        // Print the ASCII content
-        for (byte = 0; byte < 10; byte++)
-        {
-            if (offset + byte < size)
-            {
-                if (isprint(b[offset + byte]))
-                    printf("%c", b[offset + byte]);
-                else
-                    printf(".");
-            }
-        }
-
-        // End the line
-        printf("\n");
-    }
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (j = 0; j < size; j += 10)
+		{
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
+		}
+	}
 }
-
