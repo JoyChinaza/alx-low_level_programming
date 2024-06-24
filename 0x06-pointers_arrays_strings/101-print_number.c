00b9d9118557f83b7093881c811c746cceac807a
your username <nwosuchinaza22@gmail.com>
2024-06-24 05:35:56 -0700

    101 print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,34 +1,35 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer using _putchar
- * @n: The integer to be printed
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
 void print_number(int n)
 {
-    int divisor = 1;
-    int num = n;
+	unsigned int m, d, count;
 
-    /* Handle negative numbers */
-    if (n < 0)
-    {
-        _putchar('-');
-        num = -num;
-    }
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
 
-    /* Count digits and find the highest power of 10 less than num */
-    while (num / divisor >= 10)
-    {
-        divisor *= 10;
-    }
+	d = m;
+	count = 1;
 
-    /* Print each digit */
-    while (divisor != 0)
-    {
-        int digit = num / divisor;
-        _putchar(digit + '0');
-        num %= divisor;
-        divisor /= 10;
-    }
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
-
