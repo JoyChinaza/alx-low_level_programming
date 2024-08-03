d2ea73573965f5301b2b0be4c3c243114d96b3d2
your username <nwosuchinaza22@gmail.com>
2024-08-03 03:25:17 -0700

    0 print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,85 +1,24 @@
 #include "lists.h"
-#include <unistd.h> // for write function
-
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * 
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
  */
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
 
-/**
- * print_list - prints all the elements of a list_t list
- * @h: pointer to the head of the list
- *
- * Return: the number of nodes
- */
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0;
-    char *str;
-    unsigned int len;
-    const list_t *current = h;
+	size_t nelem;
 
-    while (current != NULL)
-    {
-        str = current->str;
-        len = current->len;
-
-        if (str == NULL)
-        {
-            _putchar('[');
-            _putchar('0');
-            _putchar(']');
-            _putchar(' ');
-            _putchar('(');
-            _putchar('n');
-            _putchar('i');
-            _putchar('l');
-            _putchar(')');
-            _putchar('\n');
-        }
-        else
-        {
-            _putchar('[');
-            if (len == 0)
-            {
-                _putchar('0');
-            }
-            else
-            {
-                char buf[10];
-                int i = 0;
-                while (len > 0)
-                {
-                    buf[i++] = (len % 10) + '0';
-                    len /= 10;
-                }
-                while (i > 0)
-                {
-                    _putchar(buf[--i]);
-                }
-            }
-            _putchar(']');
-            _putchar(' ');
-
-            while (*str)
-            {
-                _putchar(*str);
-                str++;
-            }
-            _putchar('\n');
-        }
-
-        current = current->next;
-        node_count++;
-    }
-
-    return node_count;
+	nelem = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
 }
 
